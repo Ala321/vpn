59ac3933641c8bbd83d1e38bbbf711cb98437b03
Ala <Rafal.Sanocki@gmail.com>
2022-10-24 21:13:25 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 sudo apt-get install strongswan
 sudo apt-get install network-manager-strongswan
-cp ipsec.conf /etc/ipsec.conf
-cp ipsec.secrets /etc/ipsec.secrets
-
+cp ipsec.conf /etc/
+cp ipsec.secrets /etc/
+cp cacert.pem /etc/ipsec.d/cacerts/
+cp caseccert.pem /etc/ipsec.d/cacerts/
 
